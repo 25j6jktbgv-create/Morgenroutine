703fc04db60b2359d26ba9a15102f386a049bc7c
25j6jktbgv-create <25j6jktbgv@privaterelay.appleid.com>
2026-05-28 12:45:24 +0200

    Update cache version in service worker

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'morgenroutine-v23';
+const CACHE = 'morgenroutine-v23t';
 const PRECACHE = ['./index.html', './manifest.json', './icon.svg'];
 
 self.addEventListener('install', e => {
